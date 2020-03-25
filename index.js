fb681523a6121c69bddf9d944053462f379932de
kegriz <rafalrudko.cmzzs@gmail.com>
2020-03-25 21:10:57 +0100

    add naive REST API params for table users

--- index.js
+++ index.js
@@ -19,7 +19,68 @@ const getUsers = async (req, res) => {
   }
 };
 
+const getUser = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM users WHERE id=($1)", [
+      req.params.id
+    ]);
+    res.status(200).json(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "UPDATE users SET name=($1) WHERE id=($2)",
+      [req.query.name, req.params.id]
+    );
+    res.send("user with id = " + req.params.id + " updated");
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+};
+
+const addUser = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("INSERT into users(name) VALUES ($1)", [
+      req.params.userName
+    ]);
+    res.send(result.rowCount + " users added");
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("DELETE FROM users WHERE id=($1)", [
+      req.params.id
+    ]);
+    res.send(result.rowCount + " users deleted");
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+};
+
 express()
   .get("/", (req, res) => res.send("express is working"))
   .get("/users", getUsers)
-  .listen(PORT || 5000, () => console.log(`Listening on ${PORT}`));
+  .get("/user/:id", getUser)
+  .put("/user/:id", updateUser)
+  .post("/user/:userName", addUser)
+  .delete("/user/:id", deleteUser)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
